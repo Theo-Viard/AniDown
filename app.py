4e982bc639671d43d03427f5986880f8236e3b2d
TViard <theoviard2701@gmail.com>
2022-11-12 13:34:42 +0100

    V1 fonctionelle (avec un csv)

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import csv
 import os
+from googleapiclient.discovery import build
+import webbrowser
+
+
 print("AniTube V0.1")
 
+youtube = build('youtube','v3',developerKey=youtube_api_key)
 lstAnimeName = []
+lstAnimeIdVid = []
 mainApp = True
 options = {
     1: "Importer d'AniList",
@@ -10,14 +16,15 @@ options = {
     3: "Importer un anime individuel",
     4: "Importer du texte brut",
 }
+
 def print_menu():
-    
     for key in options.keys():
         print (key, '--', options[key] )
 
 def fileReader():
     valid = True
     while(valid):
+        print("A l'heure actuelle, le choix d'opening n'est pas possible.")
         print("Veuillez bien a mettre le fichier dans le même répertoire que l'application")
         print("Extentions acceptées : CSV, TXT, MD")
         fileName = (input("Ecrivez le nom de votre fichier (avec l'extention) : "))
@@ -37,8 +44,29 @@ def fileReader():
                 for ligne in fileReader:
                     lstAnimeName.append(ligne[0])
             print(lstAnimeName)
-            mainApp = False
-    
+    print("Veuillez entrer votre clef Api Youtube (Elle ne sera pqs sauvegardée)")
+    print("Pour plus d'informations consultez le lien suivant : https://www.youtube.com/watch?v=MX-4aRdPkqU")
+    youtube_api_key = input()
+    urlStrList = "http://www.youtube.com/watch_videos?video_ids="
+    firstLoop = True
+    for video in lstAnimeName:
+        request = youtube.search().list(
+        part="snippet",
+        type='video',
+        maxResults=1,
+        q=video + " Opening")
+        response = request.execute()
+        if(firstLoop):
+            urlStrList += response['items'][0]['id']['videoId']
+        else:
+            urlStrList += ","+response['items'][0]['id']['videoId']
+    print(urlStrList)
+    webbrowser.open(urlStrList)
+    link = input('Veuillez copier ci dessous le lien que votre navigateur viens de vous ouvrir')
+    link += "&disable_polymer=true"
+    webbrowser.open(link)
+    print("Vous n'avez plus qu'a cliquer sur les 3 points pour créer votre playlist ;)")
+    mainApp = False
 
 while(mainApp):
     print_menu()
